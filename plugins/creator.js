2bdb87952d13214442d121b79a55b5616e15eb43
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-04-17 23:17:54 +0800

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '62895342581896', 'elyas', m)
+conn.sendContact(m.chat, '62895342581896', 'ktdprjct', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
