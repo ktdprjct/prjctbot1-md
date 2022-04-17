7ac242f8c8d5ba41036f8268df824f3ab38c5db5
ktdprjct <97012512+ktdprjct@users.noreply.github.com>
2022-04-17 18:58:55 +0800

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,13 +8,13 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;ktdprjct;;;FN:ktdprjct\nORG:ktdprjct;\nTEL;type=CELL;type=VOICE;waid=62895342581896:+62 895-3425-81896\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6285643354073', 'elyas', m)
+conn.sendContact(m.chat, '62895342581896', 'elyas', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
